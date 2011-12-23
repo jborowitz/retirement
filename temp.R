86ad2228fff105d815a6d52af8d65daa239fa55b
Jeffrey Borowitz <jeffrey@jborowitz.dyndns.org>
2011-12-23 16:14:45 -0600

    indexvalues now works
    From here I basically need to set up the arguments to work right with
    actually calling the taxsim function.  But from right now, if you give 3
    equal length vectors of indices to look up, and you define hte function
    correctly (currently the function that we're using is defined internally)
    then you'll get back partly the total function but also partly the function
    evaluated at the right values.  This should work - just have to put the
    right bits in.

--- temp.R
+++ temp.R
@@ -25,13 +25,6 @@ vapply(X=c(6,1,12,-1),FUN=inv,FUN.VALUE=0,seq(0,10,by=2))
 inv <- function(value,vec){
     #Right now I am trying to vectorize 'inv' so I can vectorize other parts
     #of the code, including indexvalue
-    #print(length(value))
-    #print(is.list(value))
-    #print(is.atomic(value))
-    #print(is.list(vec))
-    #print(is.array(vec))
-    #print(is.vector(vec))
-    print(value > max(vec))
     if (value > max(vec)){
         return(length(vec))
     }
@@ -45,11 +38,13 @@ inv <- function(value,vec){
     lowdiff <- abs(lowv-value)
     highdiff <- abs(highv-value)
     if (lowdiff < highdiff){
-        return(lowv)
+        return(low)
+        #return(lowv)
         #return(list(key=low,value=lowv))
     }
     else{
-        return(highv)
+        return(high)
+        #return(highv)
         #return(list(key=high,value=highv))
     }
 }
@@ -60,20 +55,58 @@ indexvalue <- function(aval,bval,dval){
     #discreete points a1...an, b1...bn, and c1...cn, and we want to know
     #f(aval, bval, cval), but we really need to know the index ai, bi, ci,
     #which would best approximate f(aval, bval, cval).  
-    a<-seq(1000,10000,by=1000)
-    b<-seq(3,60,by=3)
-    d<-seq(12,120,by=12)
-    e <- expand.grid(a,b,d)
-    names(e)
-    out<- e$Var1 * e$Var2 * e$Var3
-    acall <- inv(a,aval)
-    bcall <- inv(b,bval)
-    dcall <- inv(d,dval)
+    # I would like to extend the function to take 3 equal length vectors
+    # and one array representign f(a,b,c) and return a vector that is the
+    # value of f for these vectors.
+    print(length(e))
+    print(e)
+a<-seq(1000,10000,by=1000)
+b<-seq(3,60,by=3)
+d<-seq(12,120,by=12)
+e <- expand.grid(a,b,d)
+out<- e$Var1 * e$Var2 * e$Var3
+    #apply(input,2,vapply,inv,FUN.VALUE=0,
+    acall <- vapply(X=aval,FUN=inv,FUN.VALUE=0,a)
+    bcall <- vapply(X=bval,FUN=inv,FUN.VALUE=0,b)
+    dcall <- vapply(X=dval,FUN=inv,FUN.VALUE=0,d)
+    print("made it to here!")
+    print(c(acall,bcall,dcall))
+    print(c(length(acall),length(bcall),length(dcall)))
+    #acall <- inv(a,aval)
+    #bcall <- inv(b,bval)
+    #dcall <- inv(d,dval)
+    print(c(length(a),length(b),length(d)))
     output <- array(out,dim=c(length(a),length(b),length(d)))
+    print(c(a,b,d))
+    #print(output)
     #The key insight here is expand.grid(a1,a2,a3) to
     #array(.,dim=c(a1,a2,a3)) is what orders things into a 3d array
-    return(list(output=output,val=output[acall$key,bcall$key,dcall$key]))
+    #return(list(output=output,val=output[acall$key,bcall$key,dcall$key]))
+    #Used to have key and value returned from inv, and this worked
+    return(list(output=output,val=output[cbind(acall,bcall,dcall)]))
 }
-a<-indexvalue(6500,31,55)
-print(a)
-print(names(a))
+
+N <- 30
+v <- c(6500,31,55)
+q <- t(matrix(v,dim=(N,length(v))))
+mystuff<-indexvalue(array(seq(500,29*500+500,by=500)),array(rep(31,30)),array(rep(55,30)))
+print(mystuff$val)
+print(c(a$val,6500*31*55))
+# Thoughts on what's going on: I ultimately have some different ranges of x
+# values, and I will computer the tax rate for each of these x values.  One
+# thing that's likely for me to do is to expand and contract the number of
+# different x values (e.g. what if we were in Florida instead?).  At the
+# beginning of running a particular version, I will make a single call of
+# taxsim.  This is reasonable because while there is fairly high overhead
+# for the call, the overhead isnt' absolutely so large that you wouldn't
+# want to pay it once per run.  Therefore, given an individual's X's, we
+# could determine initially what variables will be changing and their
+# values.  Thus the way to start might be to take as given ranges of
+# particular values (either going to the indexvalue function or not) and
+# hard code these in.  On the other hand, if I did this, I would still
+# probably want to expand or contract the NUMBER of varying assumptions, so
+# it would be good for this to work on arbitrary numbers of columns.
+
+#The 'out' variable here is already pre-determined from my external run for
+#taxsim, as are the a, b, and c variables which went into the expand
+#operation.
