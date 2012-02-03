183fc9275b44196a7f73484e6c93d6e9ed4bd824
Jeffrey Borowitz <borowitz@econ.umd.ed>
2012-02-03 13:19:08 -0500

    remove initial calculation from ret2
    ret2 is the code, so it really doesn't need to run anything

--- ret2.R
+++ ret2.R
@@ -493,16 +493,3 @@ pension <- function(decisions, parameters){
     return(finances)
 }
 
-Rprof()
-decisions <- NULL
-decisions$retirementDate <- as.Date("2053-10-22")
-decisions$savingsRate <- .08
-decisions$estate <- 10000
-#Estate in current dollars
-decisions$T1 <- floor(as.double(decisions$retirementDate - parameters$today)/365.25) 
-decisions$returnHistory <- rep(parameters$investmentReturn,parameters$T2)
-decisions$returnHistory[parameters$t>=decisions$T1] <- parameters$retirementInvestmentReturn
-testoutput <- pension(decisions=decisions, parameters=parameters)
-print(testoutput$retirementIncome)
-Rprof(NULL)
-summaryRprof()
