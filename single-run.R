33a84ba13082fb099416b41f3f804eae21faea7f
Jeffrey Borowitz <borowitz@econ.umd.ed>
2012-02-03 13:22:50 -0500

    Copied code from calculate
    Now single-run.R will load and run a single time

--- single-run.R
+++ single-run.R
@@ -1,3 +1,58 @@
+load('taxsim.RData')
+load('Model.RData')
+source('ret2.R')
+#Define parameters
+parameters <- NULL
+parameters$ssInflation <- .024
+parameters$birthday <- as.Date('1984-11-18')
+parameters$today <- Sys.Date()
+parameters$myage <- floor(as.double(parameters$today-parameters$birthday)/365.25)
+#parameters$now <- as.numeric(format(parameters$today,"%Y"))
+parameters$T2<-97-parameters$myage
+parameters$w0<-25000
+parameters$t<-seq(1,parameters$T2)
+parameters$investmentReturn<-.04
+#investmentReturn<-.04 #This return level is from Shiller's website:
+#http://www.econ.yale.edu/~shiller/data.htm
+parameters$retirementInvestmentReturn<-.03
+parameters$inflation<-.02
+#initialIncome=log(50000)
+parameters$initialIncome=100000
+parameters$randomizeIncome <- FALSE
+parameters$seed <- 101
+#This should be a logarithmic initial income
+a <- read.csv('wages.csv',head=TRUE,sep="\t")
+#These are historical wage levels for the last 59 years for SS
+res <- line(a$Year,log(a$Index))
+#incomeIndex[a$Year] <- a$Index
+#time <- seq(min(a$Year), now+T2-1)
+#p <- exp(res$coefficients[1]  + res$coefficients[2]*time)
+#p <- 40711.61 * (1+ssInflation)^(time-2009)
+#p[1:length(a$Index)] <- a$Index
+#scale <- p/p[1]
+parameters$avgwage <- 40711.61 
+parameters$race <- "White"
+parameters$hispanic <- "Non-Hispanic"
+parameters$educ <- 20
+kmax <- 16500
+imax <- 5000
+kseries <- 500*round(kmax/500*(1+parameters$inflation)^parameters$t)
+#401k contribution limits: http://en.wikipedia.org/wiki/401(k)#Contribution_limits
+iseries <- 500*round(imax/500*(1+parameters$inflation)^parameters$t)
+parameters$IRAlimit <- iseries
+#IRA contribution limits: http://www.guidestoneretirement.org/retirementplans/contributionlimits/futurecontriblimits.aspx
+#modelling note: since my default assumption is that tax rates and brackets
+#will move with inflation, I think my plan will be to just leave these
+#contribution limits equal at a given time, and then implicitly adjust for
+#inflation by reinflating a 2008 tax situation to future dollars.
+
+#http://www.ssa.gov/oact/COLA/piaformula.html
+parameters$initwage <- 9779.44
+#parameters$bp1 <- round(180 * avgwage/initwage)
+#parameters$bp2 <- round(1085 *avgwage/initwage)
+
+
+parameters$ssType <- 'current'
 Rprof()
 decisions <- NULL
 decisions$retirementDate <- as.Date("2053-10-22")
